93f181a35c9e79603c8ce8ebd4c3196957f2792e
Beau Elliott <belliott1516@gmail.com>
2022-03-22 21:19:02 -0700

    correcting ci and adjusting styles

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-import { renderXMen } from "./render-utils.js";
-import { fetchXMen } from "./fetch-utils.js";
+import { renderXMen } from './render-utils.js';
+import { fetchXMen } from './fetch-utils.js';
 // import functions and grab DOM elements
-const homeBarEl = document.querySelector('#top-section');
-const middleSectionEl = document.querySelector('#middle-section');
+// const homeBarEl = document.querySelector('#top-section');
+// const middleSectionEl = document.querySelector('#middle-section');
 const xMenDisplayEl = document.querySelector('#bottom-section');
 // let state
 
 // set event listeners 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     const xMenList = await fetchXMen();
 
     for (let xmen of xMenList){
         const xmenDisplayed = renderXMen(xmen);
         xMenDisplayEl.append(xmenDisplayed);
     }
-
 });
   // get user input
   // use user input to update state 
