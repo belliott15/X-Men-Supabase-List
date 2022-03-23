995355a1647b0a1994fbb72f6aaf7d03c19051ce
Beau Elliott <belliott1516@gmail.com>
2022-03-22 17:10:01 -0700

    created renderOneXPerson in renderUtils

--- render-utils.js
+++ render-utils.js
@@ -17,9 +17,34 @@ export function renderXMen(xmen){
 
 export function renderOneXPerson(xmen){
     const xPersonDiv = document.createElement('div');
-    const nameEl = document.createElement('p');
-    const aliasEl = document.createElement('p');
+    const nameEl = document.createElement('h2');
+    const aliasEl = document.createElement('h2');
     const imgEl = document.createElement('img');
     const detailsEl = document.createElement('div');
-    const birthdayEl = document.createElement('div');
+    const birthdayEl = document.createElement('p');
+    const abilityEl = document.createElement('p');
+    const powerLevelEl = document.createElement('p');
+    const bioEl = document.createElement('p');
+    const affiliationsEl = document.createElement('ul');
+
+    nameEl.textContent = xmen.name;
+    aliasEl.textContent = `AKA: ${xmen.alias}`;
+    imgEl.src = xmen.image;
+    
+    birthdayEl.textContent = xmen.date_of_birth;
+    abilityEl.textContent = xmen.ability;
+    powerLevelEl.textContent = xmen.power_rating_of_ten;
+    bioEl.textContent = xmen.bio;
+    affiliationsEl.textContent = `Teams: `;
+
+    for (let team of xmen.affiliations){
+        const li = document.createElement('li');
+        li.textContent = team;
+        affiliationsEl.append(li);
+    }
+
+    detailsEl.append(birthdayEl, abilityEl, powerLevelEl, bioEl, affiliationsEl);
+    xPersonDiv.append(nameEl, aliasEl, imgEl, detailsEl);
+
+    return xPersonDiv;
 }
