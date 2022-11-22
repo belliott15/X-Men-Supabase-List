3cce99817cb97ff78d1c9c2c84603b0b281a6a01
Beau Elliott <belliott1516@gmail.com>
2022-11-22 10:06:47 -0800

    updated supabase key

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,32 +1,24 @@
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwd2Z1YXF2d2x6cnRwYXV1Z290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5Njg3MTAsImV4cCI6MTk2MzU0NDcxMH0.sUI1TaJk5GE34Q06B2tduC38-RG8NO-HoqJhGa4wrhg';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwd2Z1YXF2d2x6cnRwYXV1Z290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc5Njg3MTAsImV4cCI6MTk2MzU0NDcxMH0.sUI1TaJk5GE34Q06B2tduC38-RG8NO-HoqJhGa4wrhg';
 
 const SUPABASE_URL = 'https://cpwfuaqvwlzrtpauugot.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function fetchXMen(query){
-    if (!query){
-        const response = await client
-            .from('X-Men')
-            .select('*');
+export async function fetchXMen(query) {
+    if (!query) {
+        const response = await client.from('X-Men').select('*');
 
         return response.body;
     } else {
-        const response = await client
-            .from('X-Men')
-            .select('*')
-            .ilike('alias', `%${query}%`);
+        const response = await client.from('X-Men').select('*').ilike('alias', `%${query}%`);
 
         return response.body;
     }
 }
 
-export async function fetchOneXPerson(id){
-    const response = await client
-        .from('X-Men')
-        .select('*')
-        .match({ id: id })
-        .single();
+export async function fetchOneXPerson(id) {
+    const response = await client.from('X-Men').select('*').match({ id: id }).single();
 
     return response.body;
 }
